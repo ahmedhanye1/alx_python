a2e127ba463168d76c778d60ad2477d8db6f055a
ahmedhanye1 <ahanye9@gmail.com>
2023-07-16 09:46:04 -0700

    new3

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+word = "Holberton"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+word = "Holberton"
+print("First 3 letters: {:3}".format(word[:3]))
+print("Last 2 letters: {:2}".format(word[-2:]))
+print("Middle word: {:^5}".format(word[1:-1]))
